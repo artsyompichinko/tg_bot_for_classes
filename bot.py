8d1d2f7274581da9e6f70a82765cf2474cf7c004
artsyom_pichinko <artsyompichinko@gmail.com>
2022-12-25 16:09:14 +0300

    Бот начинается

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+bot=telebot.TeleBot('5815341263:AAFMT3W01H6vO8-f9nvpxDtVKnSaB6eK2DE')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=1)
